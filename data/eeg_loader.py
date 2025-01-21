a456998454b7289a00c7ce4ab8d5d720de938b17
Nguyen Ha <nguyenhbhcm@gmail.com>
2025-01-20 20:37:16 -0800

    Pre-process eeg data

--- data/eeg_loader.py
+++ data/eeg_loader.py
@@ -14,6 +14,20 @@ def read_file(eeg_path):
         raise ValueError(f'File extension {extension} not supported')
     return fname, raw
 
+def get_dc_channels(raw, threshold):
+    # Get the channel names and pick only DC channels
+    dc_channels = [ch for ch in raw.ch_names if 'DC' in ch]
+    dc_picks = mne.pick_channels(raw.ch_names, include=dc_channels)
+
+    signal_channels = []
+    # Check which DC channels exceed the threshold
+    for idx in dc_picks:
+        data = raw.get_data(picks=idx)  # Extract data for the channel
+        if np.any(data > threshold):  # Check if any value exceeds the threshold
+            signal_channels.append(raw.ch_names[idx])
+    print(f"DC channels with signals: {signal_channels}")
+    return signal_channels
+
 def load_eeg(eeg_path, config=None):
     """
     Load EEG data from a file and apply preprocessing steps
@@ -26,14 +40,7 @@ def load_eeg(eeg_path, config=None):
     """
     # Load EEG data    
     fname, raw = read_file(eeg_path)
-    raw.pick(picks='eeg', exclude='bads')
-
-    # Rename channels
-    raw= raw.rename_channels(config['channel_map'])
-    missing_channels = [x for x in config['channels'] if x not in raw.info['ch_names']]
-    if len(missing_channels) > 0:
-        raise ValueError(f"Missing channels: {missing_channels}")
-    raw.pick(config['channels'])
+    print(f"channels: {raw.info['ch_names']}")
 
     # Resample data
     if config.get('sfreq', False):
@@ -45,7 +52,28 @@ def load_eeg(eeg_path, config=None):
     hi_pass = config.get('h_freq', None)    
     raw.filter(lo_pass, hi_pass)
 
-    return fname, raw
+    # Rename channels
+    raw= raw.rename_channels(config['channel_map'])
+
+    # Change DC channels types
+    channel_type_mapping = {ch: 'misc' for ch in raw.info['ch_names'] if "DC" in ch}
+    raw.set_channel_types(channel_type_mapping)
+
+    # Pick channels
+    missing_channels = [x for x in config['channels'] if x not in raw.info['ch_names']]
+    if len(missing_channels) > 0:
+        raise ValueError(f"Missing channels: {missing_channels}")
+    channels = config['channels']
+    dc_channels = get_dc_channels(raw, config['dc_threshold'])
+    
+    if len(dc_channels) == 1:
+        dc_channel = dc_channels[0]
+    else:
+        raise ValueError(f"Found more than 1 DC channel: {dc_channels}")
+    channels.extend([dc_channel])
+    channels = list(set(channels))
+    raw.pick(channels)
+    return fname, raw, dc_channel
 
 def get_eeg_timestamps(raw_data):
     # Start time of the recording
@@ -86,18 +114,28 @@ def load_stimulus(event_full_path, start_time, end_time):
     else:
         raise ValueError(f"Found more than 1 stimulus activies for patient {patient_id} between {start_time} and {end_time}")
     
-    return patient_id
+    return df, patient_id
 
-def load_event(event_path, config):
-    """
-    Load event data from a file
-    param:
-        event_full_path: Path to the event file
-        config: Configuration dictionary
-    return:
-        fname: File name
-        event: Event data
-    """
-    instructions = np.array(mne.read_events(event_path))
-    print(instructions)
-    return
+def trial_start_sec(row, start_time):
+    return (row['start_time']-start_time).total_seconds()
+
+def trial_end_sec(row, start_time):
+    return (row['end_time']-start_time).total_seconds()
+
+def detect_signal_start(raw, dc_channel, trial_start_sec):
+        dc_data = raw.get_data(picks=dc_channel)
+        threshold = 2 * np.std(dc_data)
+        exceeds_threshold = np.where(dc_data[0] > threshold)[0]
+        
+        # Convert trial start time (in seconds) to sample index
+        start_sample = int(trial_start_sec * raw.info['sfreq'])
+        
+        # Find the first exceedance after the start_sample
+        signal_start_samples = exceeds_threshold[exceeds_threshold > start_sample]
+        
+        if signal_start_samples.size > 0:
+            signal_start_sample = signal_start_samples[0]
+            signal_start_time = signal_start_sample / raw.info['sfreq']
+            return signal_start_sample, signal_start_time
+        else:
+            return None, None  # No signal detected
