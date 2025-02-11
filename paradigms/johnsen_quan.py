5808da9f77c8216e6cabc5b1f305ab86fdd44926
Nguyen Ha <nguyenhbhcm@gmail.com>
2025-02-11 11:15:29 -0800

    Add plot and event funtions

--- paradigms/johnsen_quan.py
+++ paradigms/johnsen_quan.py
@@ -13,7 +13,7 @@ import matplotlib.dates as mdates
 import PyQt5
 matplotlib.use('Qt5Agg')
 
-def get_johnsen_epochs_arr(trial_start_time, trial_end_time, signal_start_time, protocol_start_time, sfreq, verbose=False):
+def get_johnsen_events(trial_start_time, trial_end_time, signal_start_time, protocol_start_time, sfreq, verbose=False):
     events = []
 
     signal_start = pd.Timestamp(protocol_start_time) + pd.to_timedelta(signal_start_time, unit="s")
@@ -33,16 +33,18 @@ def get_johnsen_epochs_arr(trial_start_time, trial_end_time, signal_start_time, 
     active_signal_start_time =  max(active_start_time, signal_start)
     curr_epoch_start_time = active_signal_start_time
     curr_epoch_end_time = curr_epoch_start_time + timedelta(seconds=2)
+    i = 1
     while curr_epoch_end_time < active_end_time:
         curr_epoch_start_sample = int((curr_epoch_start_time - protocol_start_time).total_seconds() * sfreq)
-        events.append((curr_epoch_start_sample,0,1))
+        events.append((curr_epoch_start_sample,i,1))
         curr_epoch_start_time = curr_epoch_start_time + timedelta(seconds=1)
         curr_epoch_end_time = curr_epoch_start_time + timedelta(seconds=2)
+        i+=1
 
     return np.array(events)
 
 
-def plot_epochs(events, start_time, sampling_rate, row):
+def plot_epochs(events, start_time, sampling_rate, beep_start_time):
     # Function to convert sample number to time
     def sample_to_time(sample, start_time, sampling_rate):
         return start_time + pd.to_timedelta(sample / sampling_rate, unit='s')
@@ -64,7 +66,7 @@ def plot_epochs(events, start_time, sampling_rate, row):
                 [i, i], color=color, label=label if i == 0 else "")
 
     # Add a vertical dashed line at the stimulation time (e.g., start time)
-    reference_time = row['start_time'] + timedelta(seconds=10)
+    reference_time = beep_start_time + timedelta(seconds=10)
     ax.axvline(reference_time, color='green', linestyle='--', label="Stimulation start")
 
     # Set the date format for x-axis
@@ -82,6 +84,16 @@ def plot_epochs(events, start_time, sampling_rate, row):
 
     # Show the plot
     plt.show()
+
+
+def get_johnsen_epochs(raw, patient_trial, start_time, config):
+    events = []
+    for id, row in patient_trial.iterrows():
+        events.extend(get_johnsen_events(row['start_time'], row['end_time'], row['signal_start_time'], start_time, config['sfreq']))
+    epochs = mne.Epochs(raw, events, tmin=0, tmax=2, baseline=None, preload=True)
+    active_epochs = mne.Epochs(raw, events, event_id=1, tmin=config['tmin'], tmax=config['tmax'], picks='eeg', baseline=None, preload=True)
+    reference_epochs = mne.Epochs(raw, events, event_id=0, tmin=config['tmin'], tmax=config['tmax'], picks='eeg', baseline=None, preload=True)
+    return events, epochs, active_epochs, reference_epochs
 
 def compute_log_band_power_avg(active_psd, active_epochs, freq_bands):
     # Initialize a dictionary to hold the log-transformed average power per epoch for each band
@@ -116,6 +128,25 @@ def compute_log_band_power_avg(active_psd, active_epochs, freq_bands):
 
     return log_band_power_avg
 
+def plot_log_transformed_power(active_log_band_power_avg, output_path=None, verbose=False):
+    # Plot the log-transformed power for each frequency band across epochs
+    colors = ['b', 'orange', 'r', 'c']
+    fig, axs = plt.subplots(2, 2, figsize=(15, 10))
+    axs = axs.flatten()
+
+    for i, (band, log_powers) in enumerate(active_log_band_power_avg.items()):
+        axs[i].plot(range(len(log_powers)), log_powers, label=f'{band.capitalize()} Band', marker='o', color=colors[i])
+        axs[i].set_title(f'Log-Transformed Average Power in {band.capitalize()} Band Across Epochs')
+        axs[i].set_xlabel('Epochs')
+        axs[i].set_ylabel('Log Power (µV²/Hz)')
+        axs[i].grid(True)
+
+    plt.tight_layout()
+    if output_path:
+        plt.savefig(output_path)
+    if verbose:
+        plt.show()
+
 def compute_z_scores_for_bands(ref_log_band_power_avg, active_log_band_power_avg, freq_bands):
     z_score_results = {}
 
@@ -139,3 +170,51 @@ def compute_z_scores_for_bands(ref_log_band_power_avg, active_log_band_power_avg
         }
 
     return z_score_results
+
+# Plotting Z-scores for each frequency band
+def plot_z_scores(z_score_results, epoch_ids=None, first_n_epochs=0, output_path=None, verbose=False):
+    fig, axes = plt.subplots(2, 2, figsize=(20, 12))
+    axes = axes.flatten()
+    shaded_patch = None
+
+    for idx, (band, results) in enumerate(z_score_results.items()):
+        z_scores = results['z_scores']
+        significant_increase = results['significant_increase']
+        significant_decrease = results['significant_decrease']
+
+        time = np.arange(len(z_scores))  # Assuming epochs are sequential and evenly spaced
+
+        axes[idx].plot(time, z_scores, label='Z-scores', color='blue')
+        axes[idx].axhline(1.96, color='green', linestyle='--', label='Significant threshold (+1.96)')
+        axes[idx].axhline(-1.96, color='red', linestyle='--', label='Significant threshold (-1.96)')
+
+        # Highlight significant points
+        axes[idx].scatter(time[significant_increase], z_scores[significant_increase], color='green', label='Significant increase')
+        axes[idx].scatter(time[significant_decrease], z_scores[significant_decrease], color='red', label='Significant decrease')
+
+        if epoch_ids is not None and first_n_epochs > 0:
+            shaded_indices = np.where(epoch_ids <= first_n_epochs)[0]  # Get indices where condition is met
+            # Find continuous segments
+            segments = np.split(shaded_indices, np.where(np.diff(shaded_indices) > 1)[0] + 1)
+            # Loop through segments and shade
+            for segment in segments:
+                if len(segment) > 0:
+                    axes[idx].axvspan(segment[0], segment[-1], color='blue', alpha=0.2)
+
+            shaded_patch = mpatches.Patch(color='blue', alpha=0.2, label=f"First {first_n_epochs} epochs")
+                
+        axes[idx].set_title(f"{band} band", fontsize=15)
+        axes[idx].set_xlabel("Time (epochs)", fontsize=12)
+        axes[idx].set_ylabel("Z-score", fontsize=12)
+        
+    handles, labels = axes[3].get_legend_handles_labels()
+    if shaded_patch:
+        handles.append(shaded_patch)
+        labels.append(shaded_patch.get_label())
+    fig.legend(handles, labels, loc='upper center', ncol=2, bbox_to_anchor=(0.45, 0.03, 0.05, 0.05))
+    plt.tight_layout(rect=[0, 0.9, 0, 1.02])
+    plt.suptitle("Z-scores of EEG Frequency Bands After Sound Stimulation", fontsize=15, y=0.95)
+    if output_path:
+        plt.savefig(output_path)
+    if verbose:
+        plt.show()
