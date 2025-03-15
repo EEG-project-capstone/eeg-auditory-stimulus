bd5ecb36f904ff9e5b6f12558ed93076c5f04ba2
Joobee <joobee97@gmail.com>
2025-03-15 00:20:13 -0700

    Change plot png filenames

--- eeg_auditory_stimulus/claassen_analysis.py
+++ eeg_auditory_stimulus/claassen_analysis.py
@@ -429,7 +429,7 @@ def run_analysis(subject_id, dir, edf_dir, patient_df_path, date_str):
         event_ids = df['event_id'].tolist()  # Removed addition of a final event
         instructions = np.column_stack([df['start_sample'], previous_values, event_ids])
         epochs_plt = plot_instructions_and_epochs(instructions, events)
-        save_plot(patient_folder, epochs_plt, 'instructions_epochs')
+        save_plot(patient_folder, epochs_plt, 'epochs_during_instructions')
         
         epochs = preprocess_epochs(raw, events, metadata, subject_id)
         # save_plot(patient_folder, epochs.plot(scalings='auto', n_epochs=3), 'preprocess_epochs_plot')
@@ -440,7 +440,7 @@ def run_analysis(subject_id, dir, edf_dir, patient_df_path, date_str):
         # Define cross validation
         cv = LeaveOneGroupOut()
         cv_plt = plot_cross_validation(cv, epochs, psd_data)
-        save_plot(patient_folder, cv_plt, 'cross_validation')
+        save_plot(patient_folder, cv_plt, 'cross_validation_performance')
 
         clf = define_classifier()
         # Decoding performance over time
@@ -449,17 +449,17 @@ def run_analysis(subject_id, dir, edf_dir, patient_df_path, date_str):
 
         # Topo Map
         topo_plt = plot_topo_map(clf, psd_data, epochs, bands)
-        save_plot(patient_folder, topo_plt, 'topo_map')
+        save_plot(patient_folder, topo_plt, 'EEG_spatial_patterns')
 
         # Computing cross-validated AUC scores
         mean_score, scores = compute_auc(clf, psd_data, epochs, cv, subject_id, patient_folder)
 
         # Performing permutation test
         p_value, permutation_scores, observed_score, dis_plt = permutation_test(clf, psd_data, epochs, cv, subject_id, patient_folder, n_permutations=500)
-        save_plot(patient_folder, dis_plt, 'permutation_distribution')
+        save_plot(patient_folder, dis_plt, 'permutation_test_distribution')
         
         perm_plt = plot_permutation_test(permutation_scores, scores, observed_score, subject_id, patient_folder, n_permutations=500)
-        save_plot(patient_folder, perm_plt, 'permutation_plt')
+        save_plot(patient_folder, perm_plt, 'permutation_results')
 
     except Exception as e:
         save_log(patient_folder, f"Error: {str(e)}")
