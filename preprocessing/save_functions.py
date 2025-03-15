601ec2b7ef6371d84cee339b5dae4c79a1f2fb7f
Joobee <joobee97@gmail.com>
2025-03-15 00:06:41 -0700

    Remove typo for save_log

--- preprocessing/save_functions.py
+++ preprocessing/save_functions.py
@@ -9,7 +9,7 @@ def save_log(base_dir, log_data):
     log_path = os.path.join(base_dir, filename)
     with open(log_path, "a") as log_file:  # Open file in append mode
         # timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        log_file.write(f"[{log_data}\n")  # Append timestamped log entry
+        log_file.write(f"{log_data}\n")  # Append timestamped log entry
     
 def save_plot(base_dir, fig, filename):
     """Saves a plot as a PNG file"""
